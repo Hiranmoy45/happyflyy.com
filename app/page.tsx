bcacf2043ffc4cc640c543a5a2e99ee58d8869a6
Hiranmoy <hiranmoyhs380@gmail.com>
2023-09-14 16:45:48 +0530

    commited by Hiranmoy

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="bg-gray-100">
     <div className="min-h-screen flex flex-col justify-center items-center">
-      <img src="https://www.svgrepo.com/show/426192/cogs-settings.svg" alt="Logo" className="mb-8 h-40"/>
+      <img src="https://i.ibb.co/8cXWdyf/destination.png" alt="Logo" className="mb-8 h-40 w-52"/>
       <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-center text-gray-700 mb-4">Site is under maintenance</h1>
       <p className="text-center text-gray-500 text-lg md:text-xl lg:text-2xl mb-8">We are working hard to improve the user experience. Stay tuned!</p>
       <div className="flex space-x-4">
