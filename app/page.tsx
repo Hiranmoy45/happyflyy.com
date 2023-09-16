1995d652246423045a68c5b55a18cf2491beb87e
Hiranmoy <hiranmoyhs380@gmail.com>
2023-09-16 17:33:49 +0530

    commited by Hiranmoy

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,23 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="bg-gray-100">
-    <div className="min-h-screen flex flex-col justify-center items-center">
-      <img src="https://i.ibb.co/8cXWdyf/destination.png" alt="Logo" className="mb-8 h-40 w-52"/>
-      <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-center text-gray-700 mb-4">Site is under maintenance</h1>
-      <p className="text-center text-gray-500 text-lg md:text-xl lg:text-2xl mb-8">We are working hard to improve the user experience. Stay tuned!</p>
-      <div className="flex space-x-4">
-        <Link href="#" className="bg-gray-800 hover:bg-gray-700 text-white font-bold py-3 px-6 rounded">Contact Us</Link>
-        <Link href="#" className="border-2 border-gray-800 text-black font-bold py-3 px-6 rounded">Reload</Link>
+    <div className="w-full h-screen" style={{ backgroundImage: "url('https://vojislavd.com/ta-template-demo/assets/img/coming-soon.jpg')" }}>
+      <div className="w-full h-screen flex flex-col items-center justify-between bg-black bg-opacity-70 py-8">
+        <div className="flex-1 flex flex-col items-center justify-center">
+          <div className="bg-white bg-opacity-10 px-4 py-2 rounded-xl flex items-center justify-center text-cyan-100 space-x-2 lg:space-x-4">
+
+
+          </div>
+          <h1 className="text-6xl lg:text-7xl xl:text-8xl text-gray-200 tracking-wider font-bold font-serif mt-12 text-center">Coming Soon</h1>
+          <div className="flex flex-col items-center space-y-4 mt-24">
+            <p className="text-gray-300 uppercase text-sm">We are working hard to bring you an amazing experience.</p>
+
+          </div>
+        </div>
+        <div className="w-full flex items-center justify-center">
+        
+        </div>
       </div>
     </div>
-  </main>
   )
 }
